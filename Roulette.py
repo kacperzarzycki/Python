40ee4b1e1e64b3855ae2d82e6d25607ae32ea8cd
Kacper <40382001+kacperzarzycki@users.noreply.github.com>
2018-09-04 11:33:01 +0200

    Refactor Roulette.py

--- Roulette.py
+++ Roulette.py
@@ -12,18 +12,18 @@ import random
 # Chance that green occurs is 1/37	chances that red or black occurs are 18/37
 def get_colour():
 	rand = random.randrange(1, 38)
-	if rand<=18:
+	if rand <= 18:
 		colournum = 1
-	elif rand>18 and rand<=36:
+	elif rand > 18 and rand <= 36:
 		colournum = 2
-	elif rand==37:
-		colournum=3
-	colours = {1:'Red', 2:'Black', 3:'Green'}
+	elif rand == 37:
+		colournum = 3
+	colours = {1: 'Red', 2: 'Black', 3: 'Green'}
 	return colours[colournum]
 	
 	
 while True:
-	# Get Amount of cash for start. If input is incorrect cash = 100.
+	# Get amount of cash for start. If input is incorrect cash = 100.
 	try:
 		cash = int(input("Ile pieniędzy na start?"))
 	except:
@@ -36,21 +36,19 @@ while True:
 	
 	print("Obstawiasz {}zł na kolor {}".format(bet, picked_colour)) 
 	while cash > 0:
-		#input()		# Comment that line if you don't want to click enter every tour
+		#input()	# Uncomment that line if you want to click enter every tour
 		colour = get_colour()
-
 		#If player will win: change colour, get price (bet amount), set bet to 2
-		if colour==picked_colour:
-			if picked_colour=='Red':
-				picked_colour='Black'
+		if colour == picked_colour:
+			if picked_colour == 'Red':
+				picked_colour = 'Black'
 			else:
-				picked_colour='Red'
+				picked_colour = 'Red'
 			cash += bet
 			tour = 1
 			games += 1
 			bet = 2
 			print('win')
-			
 		#If player will lose: pay bet, and increase next bet by two
 		else:
 			tour += 1
@@ -58,9 +56,6 @@ while True:
 			cash -= bet
 			bet = bet * 2
 			print('lost')
-		
-		
 		print("masz: {}zł	obstawiasz {}zł na kolor {}      tura: {} gier: {}".format(cash, bet, picked_colour, tour, games))
-		
 	input('Bankrut!')
 	input()
