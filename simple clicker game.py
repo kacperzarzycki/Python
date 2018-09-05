6c11f468650b83f42df870c6d8b7dae26c187083
Kacper <40382001+kacperzarzycki@users.noreply.github.com>
2018-09-05 08:28:10 +0200

    Add comments

--- simple clicker game.py
+++ simple clicker game.py
@@ -1,19 +1,19 @@
 from os import system 
-
+# Print player's stats
 def print_info(money, lvl, eq, eqToBuy):
 	system('cls')
 	print('[-------------------------------]\n MONEY: {}\n LEVEL: {}\n \n\n\n {}\n\n You can buy:\n'.format(money, lvl, eq))
 	for unitToBuy in eqToBuy:
 		print(unitToBuy +'\n')
+	return None
 
+	# Count amount of elements of every kind separetly and return string with results
 def count_army(eqList):
 	civilAmount = eqList.count('civil')
 	policeAmount = eqList.count('police')
 	thugAmount = eqList.count('thug')
 	return str('civil[{}]   police[{}]   thug[{}]'.format(civilAmount, policeAmount, thugAmount))
-	
-	
-	
+
 money = 0
 lvl = 1
 eqList = []
@@ -24,6 +24,7 @@ while True:
 	army = count_army(eqList)
 	print_info(money, lvl, army, eqToBuy)
 	choice = input()
+	# Check condition if player has enough money, if he does - add new eq, increase income and abjure cash
 	if choice == 'civil' and money > 49:
 		eqList += ['civil']
 		income += 2
@@ -36,6 +37,7 @@ while True:
 		eqList += ['thug']
 		income += 20
 		money -= 400
+	# If player won't pick new eq, he gets money
 	else:
 		money += income
 	
