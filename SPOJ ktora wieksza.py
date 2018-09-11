109933aa3917a8e59c6336394ba3d84f17463c52
Kacper <kacperzarzycki98@gmail.com>
2018-09-11 22:21:58 +0200

    Remove functions from try instruction

--- SPOJ ktora wieksza.py
+++ SPOJ ktora wieksza.py
@@ -1,24 +1,26 @@
 # For (0=<'a', 'b'<10) of 'a b' input compare which is grater
 
-try:
-	# separate numbers
-	def separate(word):
-		characters = list(word)
-		first_number = int(characters[0])
-		second_number = int(characters[-1])
-		return first_number, second_number
-	
-	# Compare numbers. If they are equal return 0, if first is greater return 1, if second is greater return 2
-	def compare(first_number, second_number):
-		if first_number == second_number:
-			return 0
-		elif first_number > second_number:
-			return 1
-		elif first_number < second_number:
-			return 2
-		else:
-			return None
 
+# separate numbers
+def separate(word):
+	characters = list(word)
+	first_number = int(characters[0])
+	second_number = int(characters[-1])
+	return first_number, second_number
+
+# Compare numbers. If they are equal return 0, if first is greater return 1, if second is greater return 2
+def compare(first_number, second_number):
+	if first_number == second_number:
+		return 0
+	elif first_number > second_number:
+		return 1
+	elif first_number < second_number:
+		return 2
+	else:
+		return None
+
+
+try:
 	while True:
 		first_number, second_number = separate(input('x y: '))
 		print(compare(first_number, second_number))
--- SPOJ potegi.py
+++ SPOJ potegi.py
@@ -1,18 +1,19 @@
-try:
-	# Return last number only
-	def return_last(number):
-		numbers = list(str(number))
-		return numbers[-1]
+# Return last number only
+def return_last(number):
+	numbers = list(str(number))
+	return numbers[-1]
+
+def power(base, exponent):
+	power_answer = base ** exponent
+	return power_answer
 	
-	def power(base, exponent):
-		power_answer = base ** exponent
-		return power_answer
-		
-	def get_numbers():
-		base = int(input('base: '))
-		exponent = int(input('exponent: '))
-		return base, exponent
-		
+def get_numbers():
+	base = int(input('base: '))
+	exponent = int(input('exponent: '))
+	return base, exponent
+
+
+try:
 	while True:
 		base, exponent = get_numbers()
 		answer = power(base, exponent)
