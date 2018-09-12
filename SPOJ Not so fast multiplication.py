a7b7cecd90eacdac7bf565f7f17325236652a6aa
Kacper <kacperzarzycki98@gmail.com>
2018-09-12 13:47:57 +0200

    Create max value limits

--- SPOJ Not so fast multiplication.py
+++ SPOJ Not so fast multiplication.py
@@ -3,12 +3,18 @@ import re
 try:
 	while True:
 		pairs_amount = int(input('Liczba par: '))
+		if pairs_amount > 1000:
+			raise Exception('Too high value')
 		answers = []
 		for _ in range(pairs_amount):
 			pair = input('')
 			numbers = re.findall(r'[-+]?[0-9]+', pair)
 			num1 = int(numbers[0])
 			num2 = int(numbers[1])
+			if num1 > 99999:
+				raise Exception('Too high value')
+			if num2 > 99999:
+				raise Exception('Too high value')
 			answer = num1 * num2
 			answers.append(answer)
 		for ans in answers:
